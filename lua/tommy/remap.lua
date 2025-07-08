76326001ccf400cb82371b4eb8437137d1ba5c1e
Tommy Duchesne <tduchesne@dimonoff.com>
2025-07-08 16:32:29 -0400

    add system clipboard remap

--- lua/tommy/remap.lua
+++ lua/tommy/remap.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
+vim.keymap.set({'n', 'x'}, '<leader>py', '"+y', {desc = 'Copy to clipboard'})
+vim.keymap.set({'n', 'x'}, '<leader>pp', '"+p', {desc = 'Paste clipboard text'})
