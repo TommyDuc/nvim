4b792e2ae5bddc83d5ace5e9cfb9ceffd570ebab
Tommy Duchesne <tduchesne@dimonoff.com>
2025-07-21 18:20:36 -0400

    add videre.nvim

---
+++ lua/plugins/videre.lua
@@ -0,0 +1,10 @@
+return {
+    "Owen-Dechow/nvim_json_graph_view",
+	commit = "b14fde46fcf1",
+    dependencies = {
+        "Owen-Dechow/graph_view_yaml_parser", -- Optional: add YAML support
+    },
+    opts = {
+        round_units = false
+    }
+}
