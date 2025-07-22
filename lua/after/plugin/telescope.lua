b2fae27b37b849576c7cb2fc2d11637d291af6ef
Tommy Duchesne <tommy.duchesne2@gmail.com>
2025-07-21 23:55:36 -0400

    telescope: add pl for live grep

--- lua/after/plugin/telescope.lua
+++ lua/after/plugin/telescope.lua
@@ -1,6 +1,11 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pl',
+	function()
+		builtin.live_grep({prompt_title = 'Live Grep', grep_open_files = true})
+	end
+)
 vim.keymap.set('n', '<leader>ps',
 	function()
 		builtin.grep_string({ search=vim.fn.input("Grep: ") })
