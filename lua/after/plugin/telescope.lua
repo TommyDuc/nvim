4267ee6dd864c6b488a3190192fb8f70750cf397
Tommy Duchesne <tduchesne@dimonoff.com>
2025-07-22 13:22:35 -0400

    telescope: use <leader>f<x> as the prefix (f as in find)

--- lua/after/plugin/telescope.lua
+++ lua/after/plugin/telescope.lua
@@ -1,12 +1,12 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>pl',
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fl',
 	function()
 		builtin.live_grep({prompt_title = 'Live Grep', grep_open_files = true})
 	end
 )
-vim.keymap.set('n', '<leader>ps',
+vim.keymap.set('n', '<leader>fs',
 	function()
 		builtin.grep_string({ search=vim.fn.input("Grep: ") })
 	end
