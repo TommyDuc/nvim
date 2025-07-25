11d38d5b4f5d4fe52698617b5c179692ddc468c7
Tommy Duchesne <tduchesne@dimonoff.com>
2025-07-25 17:29:13 -0400

    remap: add no insert o/O with <leader>o/O

--- lua/tommy/remap.lua
+++ lua/tommy/remap.lua
@@ -2,3 +2,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc='Open netrw'})
 vim.keymap.set({'n', 'x'}, '<leader>py', '"+y', {desc = 'Copy to clipboard'})
 vim.keymap.set({'n', 'x'}, '<leader>pp', '"+p', {desc = 'Paste clipboard text'})
+vim.keymap.set("n", "<leader>o", ':put _<cr>', {desc = "'o' without insert mode"})
+vim.keymap.set("n", "<leader>O", '-:put _<cr>', {desc = "'O' without insert mode"})
