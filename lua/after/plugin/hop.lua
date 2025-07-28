55517b38906707fdd7672bd31a55781d6e594827
Tommy Duchesne <tduchesne@dimonoff.com>
2025-07-28 12:32:24 -0400

    install hop

---
+++ lua/after/plugin/hop.lua
@@ -0,0 +1,31 @@
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+
+vim.keymap.set('', '<leader>hf', function()
+    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+end, {remap=true})
+
+vim.keymap.set('', '<leader>hF', function()
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+end, {remap=true})
+
+vim.keymap.set('', '<leader>ht', function()
+    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+end, {remap=true})
+
+vim.keymap.set('', '<leader>hT', function()
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+end, {remap=true})
+
+vim.keymap.set('', '<leader>hw', ':HopWord<CR>', {remap=true})
+
+vim.keymap.set('', '<leader>hs', ':HopPattern<CR>', {remap=true})
+
+vim.keymap.set('', '<leader>hl', ':HopLineStart<CR>', {remap=true})
+
+vim.keymap.set('', '<leader>hn', ':HopNodes<CR>', {remap=true})
+
+vim.keymap.set('', '<leader>h1', ':HopChar1<CR>', {remap=true})
+
+vim.keymap.set('', '<leader>h2', ':HopChar2<CR>', {remap=true})
+
