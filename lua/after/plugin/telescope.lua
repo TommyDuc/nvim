44e32846fe634be9a0c26a889bf4e31d31c4f5ea
Tommy Duchesne <tommy.duchesne2@gmail.com>
2025-08-12 01:02:41 -0400

    telescope: disable <leader>ff
    So we can try that fff thingny

--- lua/after/plugin/telescope.lua
+++ lua/after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+-- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+-- vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fl',
 	function()
 		builtin.live_grep({prompt_title = 'Live Grep', grep_open_files = true})
