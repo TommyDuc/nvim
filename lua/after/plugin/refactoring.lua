c4d462c616e7bc970bb165a4a24717de30a831fc
Tommy Duchesne <tduchesne@dimonoff.com>
2025-09-26 13:16:49 -0400

    refactoring: set show_success_message to true

--- lua/after/plugin/refactoring.lua
+++ lua/after/plugin/refactoring.lua
@@ -21,6 +21,6 @@ require('refactoring').setup({
     },
     printf_statements = {},
     print_var_statements = {},
-    show_success_message = false, -- shows a message with information about the refactor on success
+    show_success_message = true, -- shows a message with information about the refactor on success
                                   -- i.e. [Refactor] Inlined 3 variable occurrences
 })
