a5034de8d2075bb21c0f7352c38ccb5a1134fdef
Tommy Duchesne <tduchesne@dimonoff.com>
2025-12-03 15:04:45 -0500

    mini.surround: add rest of default config

--- lua/after/plugin/mini.lua
+++ lua/after/plugin/mini.lua
@@ -64,6 +64,14 @@ require('mini.splitjoin').setup({
 })
 
 require('mini.surround').setup({
+	-- Add custom surroundings to be used on top of builtin ones. For more
+	-- information with examples, see `:h MiniSurround.config`.
+	custom_surroundings = nil,
+
+	-- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+	highlight_duration = 500,
+
+	-- Module mappings. Use `''` (empty string) to disable one.
 	mappings = {
 		add = 'sa', -- Add surrounding in Normal and Visual modes
 		delete = 'sd', -- Delete surrounding
@@ -75,6 +83,25 @@ require('mini.surround').setup({
 		suffix_last = 'l', -- Suffix to search with "prev" method
 		suffix_next = 'n', -- Suffix to search with "next" method
 	},
+
+	-- Number of lines within which surrounding is searched
+	n_lines = 20,
+
+	-- Whether to respect selection type:
+	-- - Place surroundings on separate lines in linewise mode.
+	-- - Place surroundings on each line in blockwise mode.
+	respect_selection_type = false,
+
+	-- How to search for surrounding (first inside current line, then inside
+	-- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+	-- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
+	-- see `:h MiniSurround.config`.
+	search_method = 'cover',
+
+	-- Whether to disable showing non-error feedback
+	-- This also affects (purely informational) helper messages shown after
+	-- idle time if user input is required.
+	silent = false,
 })
 
 require('mini.bufremove').setup()
