1dae66c2a23262f054f8d96267fc9c9eee841a7f
Tommy Duchesne <tduchesne@dimonoff.com>
2026-04-02 11:28:05 -0400

    remap: add <leader>pf to copy file:line to clipboard

--- lua/tommy/remap.lua
+++ lua/tommy/remap.lua
@@ -2,5 +2,12 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc='Open netrw'})
 vim.keymap.set({'n', 'x'}, '<leader>py', '"+y', {desc = 'Copy to clipboard'})
 vim.keymap.set({'n', 'x'}, '<leader>pp', '"+p', {desc = 'Paste clipboard text'})
+vim.keymap.set("n", "<leader>pf", function()
+    local path = vim.fn.expand('%:p')
+    local line = vim.fn.line('.')
+    local location = path .. ':' .. line
+    vim.fn.setreg('+', location)
+    vim.notify('Copied: ' .. location, vim.log.levels.INFO)
+end, {desc = 'Yank file path:line to clipboard'})
 vim.keymap.set("n", "<leader>o", ':put _<cr>', {desc = "'o' without insert mode"})
 vim.keymap.set("n", "<leader>O", '-:put _<cr>', {desc = "'O' without insert mode"})
