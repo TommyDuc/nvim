7b1a1a387c3d0da53a1e3667862c5405d06f85c1
Tommy Duchesne <tduchesne@dimonoff.com>
2026-04-02 11:36:02 -0400

    remap: add <leader>pf to copy selection to clipboard

--- lua/tommy/remap.lua
+++ lua/tommy/remap.lua
@@ -9,5 +9,17 @@ vim.keymap.set("n", "<leader>pf", function()
     vim.fn.setreg('+', location)
     vim.notify('Copied: ' .. location, vim.log.levels.INFO)
 end, {desc = 'Yank file path:line to clipboard'})
+vim.keymap.set("x", "<leader>pf", function()
+    local path = vim.fn.expand('%:p')
+    local line1 = vim.fn.line('v')
+    local line2 = vim.fn.line('.')
+    -- Ensure line1 is always the smaller number
+    if line1 > line2 then
+        line1, line2 = line2, line1
+    end
+    local location = path .. ':' .. line1 .. '-' .. line2
+    vim.fn.setreg('+', location)
+    vim.notify('Copied: ' .. location, vim.log.levels.INFO)
+end, {desc = 'Yank file path:line1-line2 to clipboard'})
 vim.keymap.set("n", "<leader>o", ':put _<cr>', {desc = "'o' without insert mode"})
 vim.keymap.set("n", "<leader>O", '-:put _<cr>', {desc = "'O' without insert mode"})
